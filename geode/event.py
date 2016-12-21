60e74376646bd49b03a0585a2604b9d77095ef48
Drew Monroe <d.monroe@snet.net>
2016-12-21 09:53:34 -0500

    Add some code for the event class including event matching and merging

---
+++ geode/event.py
@@ -0,0 +1,84 @@
+import datetime
+import copy
+
+# The default duration for an event (in seconds)
+DEFAULT_DURATION = 30
+
+
+class Event(dict):
+    """This class is used to deal with events from Splunk and Postgres
+    It provides an easy way to do comparison, combining, etc."""
+
+    def __init__(self, d):
+        """Constructor; takes in a dict and copies the values to this object"""
+
+        if type(d) is not dict:
+            raise Exception('Invalid type')
+        for k in d.keys():
+            self[k] = d[k]
+
+    def match(self, e):
+        """Returns True if this event and e have no conflicting information"""
+
+        # Get all of the keys for this event
+        keys = self.keys()
+
+        # Remove the keys that we don't care about
+        if 'start' in keys:
+            keys.remove('start')
+        if 'stop' in keys:
+            keys.remove('stop')
+        if 'id' in keys:
+            keys.remove('id')
+        if 'event_type' in keys:
+            keys.remove('event_type')
+        if 'useragent' in keys:
+            keys.remove('useragent')
+        if 'os' in keys:
+            keys.remove('os')
+
+        # For all of the keys that we do care about, check to make sure that
+        # there is no conflicting evidence between the two events
+        for key in keys:
+            if e.get(key) is None or self.get(key) == e.get(key):
+                continue
+            else:
+                # There is conflicting evidence, so return
+                return False
+
+        # If we got here, there is no conflicting evidence
+        return True
+
+    def merge(self, e):
+        """Merges two events together
+        This means taking all of the values from e and all of the values from
+        self and putting them into one event
+
+        Neither e nor self are modified during this process
+        """
+
+        # if there is no stop time for either event, default it to 30 seconds
+        if not self.get('stop'):
+            self['stop'] = (self.get('start') +
+                            datetime.timedelta(seconds=DEFAULT_DURATION))
+        if not e.get('stop'):
+            e['stop'] = (e.get('start') +
+                         datetime.timedelta(seconds=DEFAULT_DURATION))
+
+        # the merged event; default to have the values of this event
+        tmp = copy.deepcopy(self)
+
+        # take the earliest start time
+        tmp['start'] = min(self.get('start'), e.get('start'))
+
+        # it's safe to assume that there are no DHCP RELEASE events, so we want
+        # to take the latest stop time possible, since if we terminate early,
+        # it won't matter which time we picked anyways
+        tmp['stop'] = max(self.get('stop'), e.get('stop'))
+
+        # copy e into the new event
+        for k in e.keys():
+            tmp[k] = e[k]
+
+        # return the merged event
+        return tmp
