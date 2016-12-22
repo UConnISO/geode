37de933b94fedec46e450aa85944929c63f814c9
Drew Monroe <d.monroe@snet.net>
2016-12-22 11:36:03 -0500

    Fix bug with checking time overlaps during matching of events
    - Added another unittest for event checking
    - Found and fixed a bug where I was not checking for time overlaps
      correctly

--- tests/test_event.py
+++ tests/test_event.py
@@ -12,17 +12,19 @@ class EventTestCase(unittest.TestCase):
         """Create various events that we will use in this file"""
 
         # this is an event from splunk
-        self.event1 = {
+        self.event1 = Event(
+                      {
                         'ip': '',
                         'mac': 'ff:ff:ff:ff:ff:ff',
                         'netid': 'djm13029',
                         'start': '2016-11-22T11:11:22',
                         'stop': '2016-11-22T11:41:22',
                         'event_type': 'wireless_authentication'
-                      }
+                      })
 
         # this is an event from postgres
-        self.event2 = {
+        self.event2 = Event(
+                      {
                         'id': 1,
                         'ip': '127.0.0.1',
                         'mac': 'ff:ff:ff:ff:ff:ff',
@@ -30,12 +32,20 @@ class EventTestCase(unittest.TestCase):
                         'start': datetime.datetime(2016, 11, 22, 11, 22),
                         'stop': datetime.datetime(2016, 11, 22, 21, 11),
                         'event_type': [1, 2]
-                      }
+                      })
+
+        self.event3 = Event(
+                      {
+                       'ip': '127.0.0.1',
+                        'start': '2016-12-12T00:00:00',
+                        'stop': '2016-12-12T00:30:00',
+                        'user_agent': 'test user agent string'
+                      })
 
     def test_event_creation(self):
         """ Test to make sure that event creation happens as we expect """
 
-        e = Event(self.event1)
+        e = self.event1
         self.assertEquals(e.get('ip'), None)
         self.assertEquals(e.get('mac'), 'ff:ff:ff:ff:ff:ff')
         self.assertEquals(e.get('netid'), 'djm13029')
@@ -47,7 +57,7 @@ class EventTestCase(unittest.TestCase):
                           set(['wireless_authentication']))
         self.assertEquals(e.get('id'), None)
 
-        e2 = Event(self.event2)
+        e2 = self.event2
         self.assertEquals(e2.get('id'), 1)
         self.assertEquals(e2.get('ip'), '127.0.0.1')
         self.assertEquals(e2.get('mac'), 'ff:ff:ff:ff:ff:ff')
@@ -58,18 +68,31 @@ class EventTestCase(unittest.TestCase):
                           datetime.datetime(2016, 11, 22, 21, 11, 00))
         self.assertEquals(e2.get('event_type'), set(['DHCPACK', 'DHCPEXPIRE']))
 
+        e3 = self.event3
+        self.assertEquals(e3.get('ip'), '127.0.0.1')
+        self.assertEquals(e3.get('mac'), None)
+        self.assertEquals(e3.get('start'),
+                          datetime.datetime(2016, 12, 12, 00, 00 ,00))
+        self.assertEquals(e3.get('stop'),
+                          datetime.datetime(2016, 12, 12, 00, 30 ,00))
+        self.assertEquals(e3.get('user_agent'), 'test user agent string')
+        self.assertEquals(e3.get('os'), None)
+
     def test_match(self):
         """ Test the match functionality of events """
-        e1 = Event(self.event1)
-        e2 = Event(self.event2)
+        e1 = self.event1
+        e2 = self.event2
+        e3 = self.event3
         self.assertTrue(e1.matches(e2))
         self.assertTrue(e1.matches(e1))
         self.assertTrue(e2.matches(e2))
+        self.assertFalse(e1.matches(e3))
+        self.assertFalse(e2.matches(e3))
 
     def test_merge(self):
         """ Test the functionality of merging of events"""
-        e1 = Event(self.event1)
-        e2 = Event(self.event2)
+        e1 = self.event1
+        e2 = self.event2
         # What the merged event should look like
         tmp = Event(
                     {
